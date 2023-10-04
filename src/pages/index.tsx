fba111b4514121d81cbd4cbe4ae95308fc41dc57
Erdem Karakaş <erdemkarakas26@gmail.com>
2023-10-04 19:55:17 +0300

    session step add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "@/src/lib/api";
@@ -15,7 +14,6 @@ import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { FormDescription, FormLabel } from "@/components/ui/form";
 import {
   Select,
   SelectContent,
@@ -25,6 +23,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ArrowBigLeft, ArrowLeft, ArrowUpFromLine, Play } from "lucide-react";
 
 const exampleData = [
   {
@@ -44,7 +43,8 @@ const exampleData = [
 ];
 
 export default function Home() {
-  const [isSesionStart, setIsSesionStart] = useState(false);
+  const [sessionStep, setSessionStep] = useState(1);
+  const [selectedTechnology, setSelectedTechnology] = useState<any>(null);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [soundsOn, setSoundsOn] = useState(true);
   const { data: technologies } = api.technology.getAll.useQuery();
@@ -52,10 +52,6 @@ export default function Home() {
     technologyId: 1,
     limit: 20,
   });
-
-  function handleTechnologyCardClick() {
-    setIsSesionStart(true);
-  }
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
@@ -71,8 +67,17 @@ export default function Home() {
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#021e6d] to-[#15162c]">
         <div className="absolute right-20 top-20">
-          <Button variant={"outline"}>
-            <Link href={"/add-question"}>Soru Ekle</Link>{" "}
+          <Button
+            className="transition duration-500 hover:scale-125"
+            variant={"outline"}
+          >
+            <Link
+              className="flex flex-row items-center justify-center text-base"
+              href={"/add-question"}
+            >
+              <ArrowUpFromLine className="mr-2 h-4 w-4" />
+              Soru Ekle
+            </Link>
           </Button>
         </div>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -83,81 +88,96 @@ export default function Home() {
             Hazırlanıyor...
           </p>
 
-          {!isSesionStart &&
+          {sessionStep == 1 &&
             technologies
               ?.filter((item) => item.name === "javascript")
               .map((item) => (
                 <TechnologyCard
-                  onClick={handleTechnologyCardClick}
+                  onClick={() => setSessionStep(2)}
                   key={item.id}
                   technology={item}
                 />
               ))}
 
-          <div className="flex flex-col space-y-4 rounded-lg bg-white p-12">
-            <RadioGroup
-              className="rounded-xl border-2 border-solid p-4"
-              defaultValue="informDuringSession"
-            >
-              <div className="flex flex-col space-y-2">
-                <Label className="text-base font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ">
-                  Soru cevaplarını
-                </Label>
-                <div className="flex flex-row items-center  space-x-2">
-                  <div className="flex items-center space-x-2">
-                    <Label className="text-xl" htmlFor="r1">
-                      Anında öğren
-                    </Label>
-                    <RadioGroupItem value="informDuringSession" id="r1" />
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="informSessionEnd" id="r2" />
-                    <Label className="text-xl" htmlFor="r2">
-                      Sonunda öğren
-                    </Label>
+          {sessionStep == 2 && (
+            <div className="flex flex-col space-y-6 rounded-lg bg-white p-12">
+              <RadioGroup
+                className="rounded-xl border-2 border-solid p-4"
+                defaultValue="informDuringSession"
+              >
+                <div className="flex flex-col space-y-2">
+                  <Label className="text-sm text-muted-foreground">
+                    Test çözüm esnasında sorular işaretlendiği anda veya sonunda
+                    öğrenmek istiyorsanız.
+                  </Label>
+                  <div className="flex flex-row items-center space-x-6">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="informDuringSession" id="r1" />
+                      <Label className="text-xl" htmlFor="r1">
+                        Soru cevaplarını anında öğren
+                      </Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="informSessionEnd" id="r2" />
+                      <Label className="text-xl" htmlFor="r2">
+                        Soru cevaplarını sonunda öğren
+                      </Label>
+                    </div>
                   </div>
                 </div>
-                <Label className="text-sm text-muted-foreground">
-                  Test çözüm esnasında sorular işaretlendiği anda veya sonunda
-                  öğrenmek istiyorsanız.
-                </Label>
-              </div>
-            </RadioGroup>
+              </RadioGroup>
 
-            <div className="flex flex-row items-center space-x-6">
-              <div className="flex items-center justify-center space-x-2 ">
-                <Label className="text-xl" htmlFor="r1">
-                  {" "}
-                  Soru sayısı
-                </Label>
-                <Select defaultValue={"20"}>
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Soru sayısı seç" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Soru sayısı</SelectLabel>
-                      <SelectItem value="10">10</SelectItem>
-                      <SelectItem value="20">20</SelectItem>
-                      <SelectItem value="30">30</SelectItem>
-                      <SelectItem value="40">40</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
+              <div className="flex flex-row items-center space-x-6 px-4">
+                <div className="flex items-center justify-center space-x-2 ">
+                  <Label className="text-xl" htmlFor="r1">
+                    {" "}
+                    Soru sayısı
+                  </Label>
+                  <Select defaultValue={"20"}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Soru sayısı seç" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Soru sayısı</SelectLabel>
+                        <SelectItem value="10">10</SelectItem>
+                        <SelectItem value="20">20</SelectItem>
+                        <SelectItem value="30">30</SelectItem>
+                        <SelectItem value="40">40</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Switch
+                    onClick={() => setSoundsOn(!soundsOn)}
+                    defaultChecked={soundsOn}
+                    id="airplane-mode"
+                  />
+                  <Label htmlFor="airplane-mode">
+                    {soundsOn ? "Sesi Açık" : "Ses kapalı"}
+                  </Label>
+                </div>
               </div>
-              <div className="flex items-center space-x-2">
-                <Switch
-                  onClick={() => setSoundsOn(!soundsOn)}
-                  defaultChecked={!soundsOn}
-                  id="airplane-mode"
-                />
-                <Label htmlFor="airplane-mode">
-                  {soundsOn ? "Sesi Açık" : "Ses kapalı"}
-                </Label>
+              <div className="flex w-full flex-row justify-between px-1">
+                <Button
+                  onClick={() => setSessionStep(1)}
+                  className="text-xl"
+                  size={"xl"}
+                >
+                  <ArrowLeft className="mr-2 h-4 w-4" /> Geri Dön
+                </Button>
+                <Button
+                  onClick={() => setSessionStep(3)}
+                  className="text-xl"
+                  size={"xl"}
+                >
+                  <Play className="mr-2 h-4 w-4" /> Başla
+                </Button>
               </div>
             </div>
-          </div>
-          {isSesionStart && (
+          )}
+          {sessionStep == 3 && (
             <div className="h-2/3 w-2/3">
               {exampleData.map((question, index) => (
                 <QuestionCard
