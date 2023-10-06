b910f5d66d64ad9b705c5cbfbcf37facf24f1a8e
Erdem Karakaş <erdemkarakas26@gmail.com>
2023-10-07 01:14:24 +0300

    responsive fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,32 +55,32 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#021e6d] to-[#15162c]">
-        {sessionStep != 3 && (
-          <div className="absolute right-20 top-20">
-            <Button
-              className="transition duration-500 hover:scale-125"
-              variant={"outline"}
-              size={"xl"}
-            >
-              <Link
-                className="flex flex-row items-center justify-center text-base"
-                href={"/add-question"}
-              >
-                <ArrowUpFromLine className="mr-2 h-4 w-4" />
-                Soru Yükle
-              </Link>
-            </Button>
-          </div>
-        )}
+      <main className="flex min-h-screen min-w-full flex-col items-center justify-center bg-gradient-to-b from-[#021e6d] to-[#15162c]">
         <div className="flex min-h-screen flex-col items-center justify-center gap-12 px-10 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             <Link href={"/"}>
               Yazılım <span className="text-[hsl(212,100%,70%)]">Soruları</span>
             </Link>
           </h1>
+          {sessionStep != 3 && (
+            <div className="flex lg:absolute lg:right-20 lg:top-20">
+              <Button
+                className="transition duration-500 hover:scale-125"
+                variant={"outline"}
+                size={"xl"}
+              >
+                <Link
+                  className="flex flex-row items-center justify-center text-base"
+                  href={"/add-question"}
+                >
+                  <ArrowUpFromLine className="mr-2 h-4 w-4" />
+                  Soru Yükle
+                </Link>
+              </Button>
+            </div>
+          )}
           <div className="flex h-full justify-center">
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid-col-1 grid gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-4">
               {sessionStep == 1 &&
                 technologies?.map(
                   (item: { id: number; name: string; alias: string }) => (
