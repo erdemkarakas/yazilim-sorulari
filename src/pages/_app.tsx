e55df343a7a452fbee32429a1c43eeb4e38d82f8
Erdem Karakaş <erdemkarakas26@gmail.com>
2023-10-13 13:31:25 +0300

    fav ico add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,24 @@ import { type AppType } from "next/app";
 import { api } from "@/src/lib/api";
 
 import "@/src/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Yazılım Soruları</title>
+        <meta name="description">
+          Yazılım dilleri ile ilgili süreli süresiz test sitesi.
+        </meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
